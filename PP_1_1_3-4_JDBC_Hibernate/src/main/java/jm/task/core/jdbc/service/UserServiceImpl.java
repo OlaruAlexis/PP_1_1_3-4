f25f6887f9e9acbc36da1c2ec3442e36d066e326
OlaruAlexis <104926664+OlaruAlexis@users.noreply.github.com>
2022-08-28 00:06:38 +0300

    Add files via upload

---
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    UserDao userDao = new UserDaoJDBCImpl();
+    public void createUsersTable() {
+        userDao.createUsersTable();
+    }
+
+    public void dropUsersTable() {
+        userDao.dropUsersTable();
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        userDao.saveUser(name, lastName, age);
+        System.out.println("Пользователь " + name + " добавлен в таблицу");
+    }
+
+    public void removeUserById(long id) throws ClassNotFoundException, NoSuchMethodException {
+        userDao.removeUserById(id);
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = userDao.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        return users;
+    }
+
+    public void cleanUsersTable() {
+        userDao.cleanUsersTable();
+    }
+}
